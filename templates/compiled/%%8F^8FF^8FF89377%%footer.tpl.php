8f32e7523562ed163b8d94668544beb7c7408028
Dyego Reis de Azevedo <dyegoreisa@gmail.com>
2009-07-19 19:11:57 +0000

    remocao de arquivos errados

--- templates/compiled/%%8F^8FF^8FF89377%%footer.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2009-07-09 03:06:18
-         compiled from ../templates/footer.tpl */ ?>
-</div>
-</body>
-</html>
--- templates/compiled/%%EF^EFA^EFABBC23%%GerenciarParametro_salvar.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2009-07-09 02:55:45
-         compiled from GerenciarParametro_salvar.tpl */ ?>
-<fieldset>
-  <legend>Cadastrar Parametro</legend>
-  <p><?php echo $this->_tpl_vars['mensagem']; ?>
-</p>
-</fieldset>
