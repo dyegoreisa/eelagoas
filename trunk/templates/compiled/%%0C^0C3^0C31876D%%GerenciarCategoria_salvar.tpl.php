77183eaaf1612e05746fe6e26927c9f41a8d8547
dyegoreisa <dyegoreisa@3ad3ed50-7491-11de-927c-01e442f92a7c>
2009-07-19 19:11:57 +0000

    remocao de arquivos errados

--- trunk/templates/compiled/%%0C^0C3^0C31876D%%GerenciarCategoria_salvar.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2009-07-09 02:41:26
-         compiled from GerenciarCategoria_salvar.tpl */ ?>
-<fieldset>
-  <legend>Cadastrar Categoria</legend>
-  <p><?php echo $this->_tpl_vars['mensagem']; ?>
-</p>
-</fieldset>
--- trunk/templates/compiled/%%0F^0F2^0F227537%%Main_run.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2009-07-04 19:08:57
-         compiled from Main_run.tpl */ ?>
-<p>Sistema de cadastro do Estudo Ecol&oacute;logico nas Lagoas da Regi&atilde;o Norte Fluminense</p>
